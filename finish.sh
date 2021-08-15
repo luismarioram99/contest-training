118748f2070987eea95de28f5a925a3aa318c961
Luis Mario Ramírez <lramirez@applaudostudios.com>
2021-08-15 12:04:03 -0600

    Finished contest 1559

---
+++ finish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git add * 
+
+
+if [ "$2" == "" ]
+then
+  git commit -m "Finished contest $1"
+  git push origin develop
+else
+  git commit -m "Finished problem $1/$2"
+  git push origin develop
+fi
