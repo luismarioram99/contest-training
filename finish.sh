9e1cdda4250297a2b801a90bfdd26043aac0098f
Luis Mario Ramírez <lramirez@applaudostudios.com>
2021-08-15 12:05:36 -0600

    Modified scripts

--- finish.sh
+++ finish.sh
@@ -2,11 +2,21 @@
 
 git add * 
 
-if [ "$2" == "" ]
+if ["$1" != ""]
 then
-  git commit -m "Finished contest $1"
+  git commit
   git push origin develop
-else
-  git commit -m "Finished problem $1/$2"
-  git push origin develop
+
+else  
+
+  if [ "$2" == "" ]
+  then
+    git commit -m "Finished contest $1"
+    git push origin develop
+  else
+    git commit -m "Finished problem $1/$2"
+    git push origin develop
+  fi
+
 fi
+
