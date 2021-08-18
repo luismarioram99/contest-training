781939cfb50b2bbd8315a30d92f216858e2fa278
Luis Mario Ramírez Peralta <luismarioram99@gmail.com>
2021-08-18 13:01:04 -0600

    Finished problem 1560/D

--- 1560/D/base.cpp
+++ 1560/D/base.cpp
@@ -10,11 +10,35 @@ typedef vector<ii> vii;
 #define F first
 #define S second
 
-vs pows;
+vector<string> pows;
 
 void solve() {
-  ll n;
-  cin >> n;
+  string s;
+  cin >> s;
+
+  int n = s.size();
+
+  int ans = INT_MAX;
+
+  for (auto &x : pows) {
+
+    int size = x.size();
+
+    int i, j, c;
+    i = j = c = 0;
+
+    while (i < n && j < size) {
+
+      if (x[j] == s[i]) {
+        j++, i++, c++;
+      } else {
+        i++;
+      }
+    }
+
+    ans = min(ans, abs(c - n) + abs(c - size));
+  }
+  cout << ans << endl;
 }
 
 int main() {
@@ -22,6 +46,12 @@ int main() {
   // make cin and cout input and output faster
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
+
+  ll k = 1;
+
+  for (int i = 0; i < 64; i++) {
+    pows.push_back(to_string(k << i));
+  }
 
   int t = 1;
   cin >> t;
