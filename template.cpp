0e13f9337e07b6c6213ea6d919809d82086ce7a1
Luis Mario Ramírez Peralta <luismarioram99@gmail.com>
2021-08-21 15:48:10 -0600

    Finished contest Added cpp snippets

--- template.cpp
+++ template.cpp
@@ -1,27 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 typedef long long ll;
 typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef vector<pl> vll;
+
 typedef vector<int> vi;
 typedef pair<int, int> ii;
 typedef vector<ii> vii;
 
-void solve(){
+#define F first
+#define S second
 
-}
+void solve() {}
 
 int main() {
-
-  //make cin and cout input and output faster
+  // make cin and cout input and output faster
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
   int t = 1;
   cin >> t;
-  while(t--){
+  while (t--)
     solve();
-  }
-
-  return 0;
 }
