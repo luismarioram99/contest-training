828f888171811be2863894e4a214020ad51e0e04
Luis Mario Ramírez Peralta <luismarioram99@gmail.com>
2021-08-21 22:31:20 -0600

    Finished contest Finished problems 1530/A and 1530/B

---
+++ 1530/A/base.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef vector<pl> vll;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+#define F first
+#define S second
+
+void solve() {
+  string s;
+  cin >> s;
+
+  int max_dig = -1;
+  for (auto x : s) {
+
+    int d = x - '0';
+    max_dig = max(max_dig, d);
+  }
+
+  cout << max_dig << endl;
+}
+
+int main() {
+  // make cin and cout input and output faster
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t = 1;
+  cin >> t;
+  while (t--)
+    solve();
+}
