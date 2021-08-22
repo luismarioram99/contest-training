79473c43fdb30da7521792262bc97425f860c6cc
Luis Mario Ramírez Peralta <luismarioram99@gmail.com>
2021-08-21 23:48:11 -0600

    Finished problem 1550/A

---
+++ 1550/A/base.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef vector<pl> vll;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+#define F first
+#define S second
+
+void solve() {
+  int s;
+  cin >> s;
+
+  int res = ceil(sqrt(s));
+  cout << res << endl;
+}
+
+int main() {
+  // make cin and cout input and output faster
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t = 1;
+  cin >> t;
+  while (t--)
+    solve();
+}
